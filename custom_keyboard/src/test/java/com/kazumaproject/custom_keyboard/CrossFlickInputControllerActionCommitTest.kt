c0366f70609a24dd85b21eed85dd698d19154310
kkrix3 <254672938+kkrix3@users.noreply.github.com>
2026-09-25 09:23:22 +0900

    test: cover held text flick and existing held action dispatch

--- custom_keyboard/src/test/java/com/kazumaproject/custom_keyboard/CrossFlickInputControllerActionCommitTest.kt
+++ custom_keyboard/src/test/java/com/kazumaproject/custom_keyboard/CrossFlickInputControllerActionCommitTest.kt
@@ -97,6 +97,79 @@ class CrossFlickInputControllerActionCommitTest {
         ), committed)
         assertEquals(true, isVisiblePopupAction(map.getValue(FlickDirection.UP)))
     }
+
+    @Test
+    fun heldTextTapAndUpCommitTheSelectedTextAndDirection() {
+        val committed = mutableListOf<Triple<KeyAction?, Boolean, FlickDirection>>()
+        val heldActions = mutableListOf<KeyAction>()
+        val listener = object : NoopCrossFlickListener() {
+            override fun onFlickCommitted(
+                fallbackAction: KeyAction?,
+                isFlick: Boolean,
+                direction: FlickDirection
+            ) {
+                committed += Triple(fallbackAction, isFlick, direction)
+            }
+
+            override fun onFlickUpAfterLongPress(
+                action: KeyAction,
+                isFlick: Boolean,
+                direction: FlickDirection
+            ) {
+                heldActions += action
+            }
+        }
+        val map = mapOf(
+            FlickDirection.TAP to FlickAction.Input("かな"),
+            FlickDirection.UP to FlickAction.Input("っ")
+        )
+
+        commitCrossFlickAction(FlickDirection.TAP, map, true, listener)
+        commitCrossFlickAction(FlickDirection.UP, map, true, listener)
+
+        assertEquals(listOf(
+            Triple(KeyAction.Text("かな"), false, FlickDirection.TAP),
+            Triple(KeyAction.Text("っ"), true, FlickDirection.UP)
+        ), committed)
+        assertEquals(emptyList<KeyAction>(), heldActions)
+    }
+
+    @Test
+    fun heldSpecialActionKeepsItsExistingLongPressCallback() {
+        val committed = mutableListOf<KeyAction?>()
+        val heldActions = mutableListOf<Triple<KeyAction, Boolean, FlickDirection>>()
+        val listener = object : NoopCrossFlickListener() {
+            override fun onFlickCommitted(
+                fallbackAction: KeyAction?,
+                isFlick: Boolean,
+                direction: FlickDirection
+            ) {
+                committed += fallbackAction
+            }
+
+            override fun onFlickUpAfterLongPress(
+                action: KeyAction,
+                isFlick: Boolean,
+                direction: FlickDirection
+            ) {
+                heldActions += Triple(action, isFlick, direction)
+            }
+        }
+
+        commitCrossFlickAction(
+            FlickDirection.TAP,
+            mapOf(FlickDirection.TAP to FlickAction.Action(KeyAction.ToggleDakuten)),
+            true,
+            listener
+        )
+
+        assertEquals(emptyList<KeyAction?>(), committed)
+        assertEquals(
+            listOf(Triple(KeyAction.ToggleDakuten, false, FlickDirection.TAP)),
+            heldActions
+        )
+    }
+
 }
 
 private open class NoopCrossFlickListener : CrossFlickInputController.CrossFlickListener {
